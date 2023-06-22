72a04de6abb6a811b71fed4e1e265e2040eff075
Nabil Sahsada Suratno <nabilsahsadacode@gmail.com>
2023-06-22 12:50:57 +0700

    update controller dan routes

--- start/routes_v1/paket.ts
+++ start/routes_v1/paket.ts
@@ -2,11 +2,11 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   //User
-  Route.get('/paket', 'PaketsController.index')
+  Route.get('/paket', 'PaketsController.index').middleware('Auth')
   Route.post('/paket', 'PaketsController.create')
-  Route.get('/paket/:id', 'PaketsController.show')
-  Route.get('/paket/jenis/:jenis', 'PaketsController.getByJenis')
+  Route.get('/paket/:id', 'PaketsController.show').middleware('Auth')
+  Route.get('/paket/jenis/:jenis', 'PaketsController.getByJenis').middleware('Auth')
   Route.put('/paket/:id', 'PaketsController.update')
-  Route.delete('/paket/:id', 'PaketsController.destroy')
+  Route.delete('/paket/:id', 'PaketsController.destroy').middleware('Auth')
   Route.get('/paket/img/:id', 'PaketsController.img')
 }).prefix('api/v1')
